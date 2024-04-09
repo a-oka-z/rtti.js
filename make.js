5782208c1944bf0da00acfee35a5bbb4d4c030e8
oka.ats@gmail.com <oka.ats@gmail.com>
2024-04-09 19:57:32 +0900

    rename transpile.js to transpiler.js

--- make.js
+++ make.js
@@ -16,7 +16,7 @@ async function asyncProc(templateFilename, bodyFilename, outputFilename ){
 
 asyncProc( 'template/index.template.cjs',      'index.js',      'lib/index.cjs'      ).then(v=>console.log(v)).catch(v=>console.error(v));
 asyncProc( 'template/index.template.mjs',      'index.js',      'lib/index.mjs'      ).then(v=>console.log(v)).catch(v=>console.error(v));
-asyncProc( 'template/transpile.template.cjs',  'transpile.js',  'lib/transpile.cjs'  ).then(v=>console.log(v)).catch(v=>console.error(v));
-asyncProc( 'template/transpile.template.mjs',  'transpile.js',  'lib/transpile.mjs'  ).then(v=>console.log(v)).catch(v=>console.error(v));
+asyncProc( 'template/transpiler.template.cjs',  'transpiler.js',  'lib/transpiler.cjs'  ).then(v=>console.log(v)).catch(v=>console.error(v));
+asyncProc( 'template/transpiler.template.mjs',  'transpiler.js',  'lib/transpiler.mjs'  ).then(v=>console.log(v)).catch(v=>console.error(v));
 asyncProc( 'template/index.test.template.cjs', 'index.test.js', 'lib/index.test.cjs' ).then(v=>console.log(v)).catch(v=>console.error(v));
 asyncProc( 'template/index.test.template.mjs', 'index.test.js', 'lib/index.test.mjs' ).then(v=>console.log(v)).catch(v=>console.error(v));
