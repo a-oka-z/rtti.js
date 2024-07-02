8248553fdf49bbb7e89e57f3cbc01c4d16e43277
Atsushi Oka <oka.ats@gmail.com>
2024-07-02 19:12:31 +0900

    remove circular references from `inspect()` (use with a special care)

--- template/index.template.mjs
+++ template/index.template.mjs
@@ -1,15 +1,44 @@
 params: body
 'use strict';
 
+/*
+ * Took from MDN then, modified.
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Cyclic_object_value
+ * Tue, 02 Jul 2024 17:54:58 +0900
+ */
+function getReplacer() {
+  const ancestors = [];
+
+  return function (key, value) {
+    if (typeof value !== "object" || value === null) {
+      if ( typeof value === 'function' ) {
+        return value.toString();
+      }
+      return value;
+    }
+
+    // `this` is the object that value is contained in,
+    // i.e., its direct parent.
+    while (ancestors.length > 0 && ancestors.at(-1) !== this) {
+      ancestors.pop();
+    }
+    if (ancestors.includes(value)) {
+      return "[Circular]";
+    }
+    ancestors.push(value);
+    return value;
+  };
+}
+
 function inspect(s) {
-  return JSON.stringify( s, (k,v)=>typeof v === 'function' ? v.toString() : v, 2 );
+  return JSON.stringify( s, getReplacer(), 2 );
 }
 
 
 
 
 
-<%=body%>
+<%=body %>
 
 export {
   fold_nargs,
